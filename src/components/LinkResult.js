44371b6e7747736bb8cec8e0258ff28f77bb195b
Samy <dev.exelight@gmail.com>
2022-03-21 17:55:17 +0100

    set : react-copy-to-clipboard

--- src/components/LinkResult.js
+++ src/components/LinkResult.js
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
 
 const LinkResult = () => {
   const [shortenLink, setShortenLink] = useState("Shorten Link");
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [copied])
+  
+
   return (
     <div className="result">
       <p>{shortenLink}</p>
-      <button>Copy to Clipboard </button>
+
+      <CopyToClipboard
+        text={shortenLink}
+        onCopy={() => setCopied(true)}
+      >
+        <button className={copied ? "copied" : ""}>Copy to Clipboard</button>
+      </CopyToClipboard>
     </div>
   )
 }
